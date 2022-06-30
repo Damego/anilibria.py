7c8c5703057bfdaadbd8b864fb58dbd4d0521bb3
Damego <danyabatueff@gmail.com>
2022-06-30 15:11:55 +0500

    chore: Добавлены примеры использования

---
+++ examples/aiogram_bot.py
@@ -0,0 +1,39 @@
+"""
+Совместное использование aiogram и anilibria.py.
+"""
+
+
+from aiogram import Bot, Dispatcher, types
+from anilibria import AniLibriaClient, TitleUpdateEvent
+
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+bot = Bot("bot token")
+dp = Dispatcher(bot)
+ani_client = AniLibriaClient(proxy="your_proxy.com:80")
+
+
+@ani_client.event()
+async def on_connect():
+    print("Anilibria connected")
+
+
+@ani_client.on_title(id=8700)
+async def on_anime(event: TitleUpdateEvent):
+    print("Вышла новая серия технолайза! (хз что это)")
+
+
+@dp.message_handler()
+async def random(message: types.Message):
+    if message.text != "!random":
+        return
+    title = await ani_client.get_random_title()
+    name = title.names["ru"]
+    await message.answer(name)
+
+
+ani_client.startwith(dp.start_polling())  # ! Not executor.start_polling(dp)
---
+++ examples/disnake_bot.py
@@ -0,0 +1,38 @@
+"""
+Совместное использование disnake и anilibria.py.
+Вместо disnake может быть любой другой форк `discord.py` и сам `discord.py`
+"""
+
+
+from anilibria import AniLibriaClient, TitleUpdateEvent
+import disnake
+from disnake.ext import commands
+
+
+bot = commands.InteractionBot()
+client = AniLibriaClient(proxy="your_proxy.com:80")
+
+
+@bot.event
+async def on_ready():
+    print("Disnake bot ready!")
+
+
+@client.event()
+async def on_connect():
+    print("connected")
+
+
+@client.on_title(id=8700)
+async def on_anime(event: TitleUpdateEvent):
+    print("Вышла новая серия технолайза! (хз что это)")
+
+
+@bot.slash_command(name="random")
+async def random(interaction: disnake.CommandInteraction):
+    title = await client.get_random_title()
+    name = title.names["ru"]
+    await interaction.send(name)
+
+
+client.startwith(bot.start("bot token"))  # ! Not bot.run()
