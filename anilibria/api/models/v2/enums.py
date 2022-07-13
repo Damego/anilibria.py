b053cbc1d6cce632e787f5f0facfaac2a02a943d
Damego <danyabatueff@gmail.com>
2022-07-13 10:13:16 +0500

    docs: Добавлено описание для Enum's

--- anilibria/api/models/v2/enums.py
+++ anilibria/api/models/v2/enums.py
@@ -27,14 +27,23 @@ class AttrEnum(Enum):
 
 
 class StrEnum(str, AttrEnum):
+    """
+    ``enum.IntEnum``, но для строк и возвращает ``None``, если значение не найдено
+    """
     ...
 
 
 class IntEnum(int, AttrEnum):
+    """
+    Похож на ``enum.IntEnum``, но возвращает ``None``, если значение не найдено
+    """
     ...
 
 
 class StatusCode(IntEnum):
+    """
+    Представляет текущий статус тайтла
+    """
     AT_WORK = 1
     FINISHED = 2
     HIDDEN = 3
@@ -42,6 +51,9 @@ class StatusCode(IntEnum):
 
 
 class TitleType(IntEnum):
+    """
+    Представляет тип тайтла
+    """
     FILM = 0
     TV = 1
     OVA = 2
@@ -50,6 +62,9 @@ class TitleType(IntEnum):
 
 
 class SeasonCode(IntEnum):
+    """
+    Представляет код сезона
+    """
     EMPTY = 0
     WINTER = 1
     SPRING = 2
@@ -59,24 +74,36 @@ class SeasonCode(IntEnum):
 
 
 class RSSType(StrEnum):
+    """
+    Представляет тип ответа на ``client.get_rss()``
+    """
     RSS = "rss"
     ATOM = "atom"
     JSON = "json"
 
 
 class DescriptionType(StrEnum):
+    """
+    Представляет тип описания тайтла
+    """
     HTML = "html"
     PLAIN = "plain"
     NO_VIEW_ORDER = "no_view_order"
 
 
 class Include(StrEnum):
+    """
+    Представляет запрашиваемый контент, который недоступен по умолчанию
+    """
     RAW_POSTER = "raw_poster"
     RAW_TORRENT = "raw_torrent"
     TORRENT_META = "torrent_meta"
 
 
 class PlayListType(StrEnum):
+    """
+    Представляет тип плейлиста
+    """
     ARRAY = "array"
     LIST = "array"
     OBJECT = "object"
