2aa6b663ae82a81512cc1a6e84cbd63c994235f2
Damego <danyabatueff@gmail.com>
2022-12-12 20:03:11 +0500

    refactor: добавление `misc.py` из папки v2

---
+++ anilibria/api/models/misc.py
@@ -0,0 +1,29 @@
+from .title import Title
+from .attrs_utils import define
+
+
+__all__ = ["Schedule", "YouTubeData", "SeedStats"]
+
+
+@define()
+class Schedule:
+    day: int
+    list: list[Title]
+
+
+@define()
+class YouTubeData:
+    id: int
+    title: str
+    image: str
+    youtube_id: str
+    timestamp: int
+    comments: int
+    views: int
+
+
+@define()
+class SeedStats:
+    downloaded: int
+    uploaded: int
+    user: str
