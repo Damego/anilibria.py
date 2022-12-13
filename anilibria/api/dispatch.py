3b6003c9a6837d79e8b60be7b7da81075add95a3
Damego <danyabatueff@gmail.com>
2022-12-13 11:36:02 +0500

    refactor!: rename `EventDispatcher` to `Dispatch`

--- anilibria/api/dispatch.py
+++ anilibria/api/dispatch.py
@@ -6,7 +6,7 @@ from .models.attrs_utils import define, field, DictSerializer
 
 
 log = getLogger("anilibria.dispatch")
-__all__ = ["Event", "EventDispatcher"]
+__all__ = ["Event", "Dispatch"]
 
 
 @define()
@@ -15,22 +15,22 @@ class Event(DictSerializer):
     data: Optional[dict] = field(factory=dict)  # Extra data
 
 
-class EventDispatcher:
+class Dispatch:
     def __init__(self) -> None:
         self.loop = get_event_loop()
-        self.events: Dict[str, List[Event]] = {}
+        self._events: Dict[str, List[Event]] = {}
 
-    def _dispatch(self, coro: Coroutine):
+    def _call(self, coro: Coroutine):
         self.loop.create_task(coro)
 
     def dispatch(self, name: str, *args, **kwargs):
         log.debug(f"Dispatching event {name}")
-        for event_data in self.events.get(name, []):
-            self._dispatch(event_data.coro(*args, **kwargs))
+        for event_data in self._events.get(name, []):
+            self._call(event_data.coro(*args, **kwargs))
 
     def add_event(self, name: str, event: Event):
-        events = self.events.get(name, [])
+        events = self._events.get(name, [])
         events.append(event)
 
-        self.events[name] = events
-        log.debug(f"Added coro to {name} event. Total coros for this event: {self.events[name]}")
+        self._events[name] = events
+        log.debug(f"Added coro to {name} event. Total coros for this event: {self._events[name]}")
