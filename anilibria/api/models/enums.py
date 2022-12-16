7380f3858ea8ee593e39a9f4450f893f9a95a59f
Damego <danyabatueff@gmail.com>
2022-12-16 17:49:50 +0500

    refactor(enums): move duplicate code to func

--- anilibria/api/models/enums.py
+++ anilibria/api/models/enums.py
@@ -1,7 +1,5 @@
 from enum import Enum
-from typing import TypeVar
-
-T = TypeVar("T")
+from typing import Type, Any
 
 __all__ = [
     "StrEnum",
@@ -16,30 +14,31 @@ __all__ = [
 ]
 
 
-class StrEnum(str, Enum):
-    @classmethod
-    def _missing_(cls, value: str):
-        new = str.__new__(cls)
-        new._name_ = f"UNKNOWN-ENUM-{value}"
-        new._value_ = value
+def _cursed_enum(cls: Type[Enum], obj: type, value: Any) -> Enum:
+    new = obj.__new__(cls)  # type: ignore
+    new._name_ = f"UNKNOWN: {value}"
+    new._value_ = value
 
-        return cls._value2member_map_.setdefault(value, new)
-
-    def __str__(self):
-        return str(self.value)
+    return cls._value2member_map_.setdefault(value, new)
 
 
 class IntEnum(int, Enum):
+    """Enum where members must be ints"""
+
     @classmethod
-    def _missing_(cls, value: int):
-        new = int.__new__(cls)
-        new._name_ = f"UNKNOWN-ENUM-{value}"
-        new._value_ = value
+    def _missing_(cls, value: int) -> Enum:
+        return _cursed_enum(cls, int, value)
 
-        return cls._value2member_map_.setdefault(value, new)
 
-    def __int__(self) -> int:
-        return int(self.value)
+class StrEnum(str, Enum):
+    """Enum where members must be strings"""
+
+    @classmethod
+    def _missing_(cls, value: str) -> Enum:
+        return _cursed_enum(cls, str, value)
+
+    def __str__(self):
+        return self.value
 
 
 class StatusCode(IntEnum):
